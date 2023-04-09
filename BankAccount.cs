6d0cb1bdb3583a01311f6ea5e35b2ffc36f80550
Chlupus <purkertovam@gmail.com>
2023-04-09 12:38:50 +0200

    BankAccount class

---
+++ BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    internal class BankAccount
+    {
+        private uint accountNumber;
+        private string ownerName;
+        private int accountBalance;
+
+        public BankAccount(uint accountNumber, string ownerName, int accountBalance)
+        {
+            this.accountNumber = accountNumber;
+            this.ownerName = ownerName;
+            this.accountBalance = accountBalance;
+        }
+
+    }
+}
