5d8fc6b058171372784054b9a283b42bcd4cf3e2
Chlupus <purkertovam@gmail.com>
2023-04-09 12:56:45 +0200

    CheckInput library

---
+++ CheckInput/Check.cs
@@ -0,0 +1,53 @@
+﻿namespace CheckInput
+{
+    public class Check
+    {
+        public static string NotEmptyString()
+        {
+            bool checkString = false;
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("You must write a text");
+                    Console.WriteLine("Try it again:");
+                }
+                else
+                    checkString = true;
+            } while (checkString == false);
+            return input;
+        }
+
+
+        public static int PositiveIntCheck()
+        {
+            int option;
+            bool checkInt = false;
+            do
+            {
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out option))
+                {
+                    if (option > 0)
+                    {
+                        checkInt = true;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("The inserted integer must be positive\n");
+                        Console.WriteLine("Try it again:");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input must be a positive integer\n");
+                    Console.WriteLine("Try it again:");
+                }
+            } while (checkInt == false);
+            return option;
+        }
+    }
+}
