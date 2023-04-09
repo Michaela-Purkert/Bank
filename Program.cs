490541fffe1e1dd4cb6b03e7bf967decb834fa05
Chlupus <purkertovam@gmail.com>
2023-04-09 11:46:27 +0200

    New menu

--- Program.cs
+++ Program.cs
@@ -1,1 +1,54 @@
-﻿
+﻿using ConsoleTools;
+using System.Threading.Channels;
+
+void Action1()
+{
+    Console.WriteLine("Action 1");
+    Console.ReadKey();
+}
+
+void Action2()
+{
+    Console.WriteLine("Action 2");
+    Console.ReadKey();
+}
+
+void Action3()
+{
+    Console.WriteLine("Action 3");
+    Console.ReadKey();
+}
+
+var subMenu = new ConsoleMenu(args, level: 1)
+        .Add("Sub_One", () => Action1())
+        .Add("Sub_Two", () => Action2())
+        .Add("Sub_Three", () => Action3())
+        .Add("Sub_Close", ConsoleMenu.Close)
+        .Configure(config =>
+        {
+            config.Selector = "--> ";
+            config.EnableFilter = false;
+            config.Title = "Submenu";
+            config.EnableBreadcrumb = true;
+            config.WriteBreadcrumbAction = titles => Console.WriteLine(string.Join(" / ", titles));
+        });
+
+var menu = new ConsoleMenu(args, level: 0)
+  .Add("One", () => Action1())
+  .Add("Two", () => Action2())
+  .Add("Three", () => Action3())
+  .Add("Sub", subMenu.Show)
+  //.Add("Change me", (thisMenu) => thisMenu.CurrentItem.Name = "I am changed!")
+  .Add("Close", ConsoleMenu.Close)
+  //.Add("Action then Close", (thisMenu) => { SomeAction("Close"); thisMenu.CloseMenu(); })
+  .Add("Exit", () => Environment.Exit(0))
+  .Configure(config =>
+  {
+      config.Selector = "--> ";
+      config.EnableFilter = false;
+      config.Title = "Welcome to the Bank appliacation\nChoose from options below";
+      config.EnableWriteTitle = false;
+      config.EnableBreadcrumb = true;
+  });
+
+menu.Show();
